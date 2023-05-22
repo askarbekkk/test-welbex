d27f0ab22134daf93ba1056593b67cd7c0f545eb
askarbekkk <askarkanatbekovic@gmail.com>
2023-05-22 16:44:54 +0600

    changes

--- index.js
+++ index.js
@@ -6,11 +6,52 @@ const PORT = process.env.PORT ||  5000;
 const mongoose = require("mongoose")
 const authRouter = require("./routes/authRoutes")
 const swaggerDocs = require("./swagger")
+const {Express, Request, Response} = require("express")
+const swaggerJsDoc = require("swagger-jsdoc")
+const swaggerUi = require("swagger-ui-express")
+const {version} = require("./package.json")
+
+
+
+
+
+const options = {
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "REST API Docs",
+            version
+        },
+        // servers: ["https://test-welbex.herokuapp.com"],
+        servers: ["http://localhost:5000"],
+
+    },
+
+    apis: ["./routes/*.js", "./models/*.js"]
+}
+const swaggerSpec = swaggerJsDoc(options)
+
 app.use(express.json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec))
+
+
+// Routes
+/**
+ * @swagger
+ * /posts:
+ * get:
+ *    describe: Used to request all posts of a certain user
+ *    responses:
+ *      "200":
+ *        description:  A successfull response
+ *
+ *
+ * **/
+
 
 app.use("/post_smth", authRouter)
 
-//mongodb+srv://askarbekk:<password>@cluster0.llpikth.mongodb.net/?retryWrites=true&w=majority
 const start  = async () => {
     try{
         await mongoose.connect(`mongodb+srv://askarbekk:12345@cluster0.llpikth.mongodb.net/?retryWrites=true&w=majority`)
--- swagger.js
+++ swagger.js
@@ -12,7 +12,7 @@ const options = {
             version
         },
      servers: [{
-            api: "https://test-welbex.herokuapp.com"
+            url: "https://test-welbex.herokuapp.com"
      }]
     },
 
